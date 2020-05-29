393454c453ac01bdc491d0e9d5df1b632de516c7
Gabriel Gigante <gabriel_gigante@outlook.com>
2020-05-29 14:03:31 -0300

    Using class transform to customize entities responses

--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: 'b0edb20dc15d88f6d01a5e244c84e617',
+    secret: process.env.APP_SECRET,
     expiresIn: '1d',
   },
 };
--- backend/src/modules/users/infra/http/controllers/ProfileController.ts
+++ backend/src/modules/users/infra/http/controllers/ProfileController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
 import ShowProfileService from '@modules/users/services/ShowProfileServices';
@@ -29,8 +30,6 @@ export default class ProfileController {
       password,
     });
 
-    delete user.password;
-
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 }
