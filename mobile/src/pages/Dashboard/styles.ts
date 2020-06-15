165bb9f128c6db047568fd7a19da44cd84178209
Gabriel Gigante <gabriel_gigante@outlook.com>
2020-06-14 23:20:45 -0300

    Dashboard header

---
+++ mobile/src/pages/Dashboard/styles.ts
@@ -0,0 +1,42 @@
+import styled, { css } from 'styled-components/native';
+import { Platform } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Header = styled.View`
+  padding: 24px;
+
+  ${Platform.OS === 'ios' &&
+  css`
+    padding-top: ${getStatusBarHeight() + 24}px;
+  `}
+
+  background: #28262e;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const HeaderTitle = styled.Text`
+  color: #f4ede8;
+  font-size: 20px;
+  font-family: 'RobotoSlab-Regular';
+  line-height: 28px;
+`;
+
+export const UserName = styled.Text`
+  color: #ff9000;
+  font-family: 'RobotoSlab-Medium';
+`;
+
+export const ProfileButton = styled.TouchableOpacity``;
+
+export const UserAvatar = styled.Image`
+  width: 56px;
+  height: 56px;
+  border-radius: 28px;
+`;
