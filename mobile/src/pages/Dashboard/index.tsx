1d6282334c63fe25db163ef679bc55f244c469e6
Gabriel Gigante <gabriel_gigante@outlook.com>
2020-06-16 16:37:17 -0300

    Recover the app

---
+++ mobile/src/pages/Dashboard/index.tsx
@@ -0,0 +1,39 @@
+import React, { useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
+
+import { useAuth } from '../../hooks/Auth';
+
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  ProfileButton,
+  UserAvatar,
+} from './styles';
+
+const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+  const { navigate } = useNavigation();
+
+  const navigateToProfile = useCallback(() => {
+    navigate('Profile');
+  }, [navigate]);
+
+  return (
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem vindo, {'\n'}
+          <UserName>{user.name}</UserName>
+        </HeaderTitle>
+
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar source={{ uri: user.avatar_url }} />
+        </ProfileButton>
+      </Header>
+    </Container>
+  );
+};
+
+export default Dashboard;
